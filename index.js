2634a13b39a38bc4e279db1ef3f7b79879b6f601
Roger Chin <rogerchin85@gmail.com>
2018-03-02 13:51:01 -0800

    saving work

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ var pool = new pg.Pool({
     port: process.env.DB_PORT
 });
 
+// initialize body parser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 // Initializing PUG template
 app.set('view engine', 'pug');
 app.set('views', ['templates', 'templates/inc', 'templates/blocks', 'templates/dev']);
@@ -42,27 +47,51 @@ app.use('/fonts', express.static('fonts'));
 // Routes
 // Get pages
 app.get("/", function (req, resp) {
-    resp.render('blocks/login');
+    if (req.session.username) {
+        resp.redirect('/profile');
+    } else {
+        resp.render('blocks/login');
+    }
 });
 
 app.get('/profile', function(req, resp) {
-    resp.render('blocks/profile');
+    if (req.session.username) {
+        resp.render('blocks/profile', {user: req.session});
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/edit-profile', function(req, resp) {
-    resp.render('blocks/edit-profile')
+    if (req.session.username) {
+        resp.render('blocks/edit-profile');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/postings', function(req, resp) {
-    resp.render('blocks/postings');
+    if (req.session.username) {
+        resp.render('blocks/postings');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/my-applications', function(req, resp) {
-    resp.render('blocks/my-applications');
+    if (req.session.username) {
+        resp.render('blocks/my-applications');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/my-posts', function(req, resp) {
-    resp.render('blocks/my-posts');
+    if (req.session.username) {
+        resp.render('blocks/my-posts');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/posting-details', function(req, resp) {
@@ -70,19 +99,60 @@ app.get('/posting-details', function(req, resp) {
 });
 
 app.get('/register', function(req, resp) {
-    resp.render('blocks/register');
+    if (req.session.username) {
+        resp.render('blocks/register');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/inbox', function(req, resp) {
-    resp.render('blocks/inbox');
+    if (req.session.username) {
+        resp.render('blocks/inbox');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/message', function(req, resp) {
-    resp.render('dev/message');
+    if (req.session.username) {
+        resp.render('dev/message');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
 });
 
 app.get('/create-post', function(req, resp) {
-    resp.render('blocks/create-post');
+    if (req.session.username) {
+        resp.render('blocks/create-post');
+    } else {
+        resp.render('blocks/login', {message: "You're not logged in"});
+    }
+});
+
+// Authentications
+app.post('/login', function(req, resp) {
+    pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [req.body.username, req.body.password], function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+
+        if (result != undefined && result.rowCount > 0) {
+            req.session = result.rows[0];
+            resp.redirect('/profile')
+        }
+    });
+});
+
+app.get('/logout', function(req, resp) {
+    req.session = null;
+
+    resp.redirect('/');
+});
+
+// Create
+app.post('/new_post', function(req, resp) {
+    console.log(req.body);
 });
 
 // Server listening
